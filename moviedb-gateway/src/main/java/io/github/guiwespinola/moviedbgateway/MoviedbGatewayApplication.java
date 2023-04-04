882cf34af9c32471ff7492777ac8c1c43f1ad2bc
Guilherme Wanderley Espinola <gwanderleyespinola@gmail.com>
2023-04-04 09:24:45 -0300

    load-balancing on gateway

--- moviedb-gateway/src/main/java/io/github/guiwespinola/moviedbgateway/MoviedbGatewayApplication.java
+++ moviedb-gateway/src/main/java/io/github/guiwespinola/moviedbgateway/MoviedbGatewayApplication.java
@@ -3,7 +3,10 @@ package io.github.guiwespinola.moviedbgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
@@ -13,6 +16,13 @@ public class MoviedbGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviedbGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/api/v1/movies/**").uri("lb://json-parser"))
+				.build();
 	}
 
 }
